a512a95e839c7147124ec6b02d06beeca66c76e3
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-10-15 14:26:15 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double altura, peso, imc;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("Altura (m)..: ");
+altura = Convert.ToDouble(Console.ReadLine());
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("Peso (kg)...: ");
+peso = Convert.ToDouble(Console.ReadLine());
+Console.ResetColor();
+
+imc = peso / Math.Pow(altura, 2);
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"\nIMC: {imc:N1} kg/m²");
+Console.ResetColor();
